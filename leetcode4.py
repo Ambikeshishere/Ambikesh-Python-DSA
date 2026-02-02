45e296b76901052854b476b4899d04d04e8c404f
Ambikesh Srivastava <abhay2004raj15@gmail.com>
2026-02-02 17:26:23 +0530

    updated

---
+++ leetcode4.py
@@ -0,0 +1,28 @@
+#Given a string s, return the longest palindromic substring in s.
+
+def longestPalindrome(s: str) -> str:
+    if not s:
+        return ""
+
+    start = end = 0
+
+    def expand(l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return l + 1, r - 1
+
+    for i in range(len(s)):
+        l1, r1 = expand(i, i)       # odd length
+        l2, r2 = expand(i, i + 1)   # even length
+
+        if r1 - l1 > end - start:
+            start, end = l1, r1
+        if r2 - l2 > end - start:
+            start, end = l2, r2
+
+    return s[start:end + 1]
+
+
+longestPalindrome("babad")  
+longestPalindrome("cbbd")   
